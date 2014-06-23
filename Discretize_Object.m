8a490e49d25f2bd8d68b82ebd48619a86ad2d704
Jui <jhwang.cornell@gmail.com>
2014-06-22 21:55:30 -0400

    main_v4 construction in progress

---
+++ Discretize_Object.m
@@ -0,0 +1,19 @@
+function Discretize_Object(NumSource)
+
+%%%%% no idea how to do this %%%%%
+%
+% Need: 
+%   Some systematic way to split the data and
+%   geometry into several parts according to 
+%   the discretize dictionary prescribed with 
+%   the object 
+%
+% Something that might help: 
+%   the object is usually slender and therefore 
+%   has one longitudinal direction. like a club 
+%   or a sword or something like that
+
+
+
+
+end
--- tmp.m
+++ tmp.m
@@ -1,40 +1,19 @@
-% Calculate the swing sound
-[T,Pv_Re120            ,X,U,vl,O,S_k_mon] = main_v2([0,10,0],16000,Tex_Re120            );
-[T,Pv_Re1200_Dobashi   ,X,U,vl,O,S_k_mon] = main_v2([0,10,0],16000,Tex_Re1200_Dobashi   );
-[T,Pv_Re12000_ke90000  ,X,U,vl,O,S_k_mon] = main_v2([0,10,0],16000,Tex_Re12000_ke90000  );
-[T,Pv_Re12000_kw90000  ,X,U,vl,O,S_k_mon] = main_v2([0,10,0],16000,Tex_Re12000_kw90000  );
-[T,Pv_Re12000_dns45000 ,X,U,vl,O,S_k_mon] = main_v2([0,10,0],16000,Tex_Re12000_dns45000 );
-[T,Pv_Re12000_dns180000,X,U,vl,O,S_k_mon] = main_v2([0,10,0],16000,Tex_Re12000_dns180000);
+function tmp()
 
-player_Re120             = audioplayer(Pv_Re120            ,16000);
-player_Re1200_Dobashi    = audioplayer(Pv_Re1200_Dobashi   ,16000);
-player_Re12000_ke90000   = audioplayer(Pv_Re12000_ke90000  ,16000);
-player_Re12000_kw90000   = audioplayer(Pv_Re12000_kw90000  ,16000);
-player_Re12000_dns45000  = audioplayer(Pv_Re12000_dns45000 ,16000);
-player_Re12000_dns180000 = audioplayer(Pv_Re12000_dns180000,16000);
+Table = struct('name','initialize');
+Table
+set_trait(Table);
 
-save('../Rendered_data/Swing_CompareFluidModel.mat')
-
-disp('Finish Swing construct')
+Table
 
 
-%  Calculate the wind sound
-[T,Pv_Re120            ,X,U,vl,O,S_k_mon] = main_v3([0,0,0],16000,Tex_Re120            );
-[T,Pv_Re1200_Dobashi   ,X,U,vl,O,S_k_mon] = main_v3([0,0,0],16000,Tex_Re1200_Dobashi   );
-[T,Pv_Re12000_ke90000  ,X,U,vl,O,S_k_mon] = main_v3([0,0,0],16000,Tex_Re12000_ke90000  );
-[T,Pv_Re12000_kw90000  ,X,U,vl,O,S_k_mon] = main_v3([0,0,0],16000,Tex_Re12000_kw90000  );
-[T,Pv_Re12000_dns45000 ,X,U,vl,O,S_k_mon] = main_v3([0,0,0],16000,Tex_Re12000_dns45000 );
-[T,Pv_Re12000_dns180000,X,U,vl,O,S_k_mon] = main_v3([0,0,0],16000,Tex_Re12000_dns180000);
-
-player_Re120             = audioplayer(Pv_Re120            ,16000);
-player_Re1200_Dobashi    = audioplayer(Pv_Re1200_Dobashi   ,16000);
-player_Re12000_ke90000   = audioplayer(Pv_Re12000_ke90000  ,16000);
-player_Re12000_kw90000   = audioplayer(Pv_Re12000_kw90000  ,16000);
-player_Re12000_dns45000  = audioplayer(Pv_Re12000_dns45000 ,16000);
-player_Re12000_dns180000 = audioplayer(Pv_Re12000_dns180000,16000);
-
-save('../Rendered_data/Wind_CompareFluidModel.mat')
-
-disp('Finish Wind construct')
+end
 
 
+function set_trait(Table)
+
+Table
+Table.name = 'Hello World!';
+Table
+
+end
